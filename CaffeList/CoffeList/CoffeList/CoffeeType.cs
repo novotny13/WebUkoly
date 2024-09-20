60af72fde5a26db1250fe47409cc6ad3688f47dc
novotny13 <127189062+novotny13@users.noreply.github.com>
2024-09-20 21:14:01 +0200

    Add files via upload

---
+++ CaffeList/CoffeList/CoffeList/CoffeeType.cs
@@ -0,0 +1,15 @@
+﻿namespace CoffeeApp
+{
+    public class CoffeeType
+    {
+        public CoffeeType(string iD, string typ)
+        {
+            ID = iD;
+            Typ = typ;
+        }
+
+        public string ID { get; set; }
+        public string Typ { get; set; }
+    }
+
+}
---
+++ CaffeList/CoffeList/CoffeList/Person.cs
@@ -0,0 +1,21 @@
+﻿
+namespace CoffeeApp
+{
+    public class Person
+    {
+        public Person(string ID, string name)
+        {
+            ID = ID;
+            Name = name;
+        }
+
+        public string ID { get; set; }
+        public string Name { get; set; }
+
+        
+        public override string? ToString()
+        {
+            return ID + Name;
+        }
+    }
+}
